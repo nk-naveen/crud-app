4760bdfebbaee434fb0612ff57774dfe400cec46
nkninja <83780886+nkninja@users.noreply.github.com>
2022-03-21 11:52:40 +0530

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,19 +1,19 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
-// Initial State
+
 const initialState = {
   users: []
 }
 
-// Create Context
+
 export const GlobalContext = createContext(initialState);
 
-// Provider Component
+
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  // Actions
+  
   const removeUser = (id) => {
     dispatch({
       type: 'REMOVE_USER',
